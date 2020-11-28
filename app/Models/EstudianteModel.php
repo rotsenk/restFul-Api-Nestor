a56ff5a9c77a3560fd2ca5d9a55c836fd1113fac
Nestor Rivas <stanley.95@hotmail.com>
2020-11-28 00:13:49 -0600

    regex

--- app/Models/EstudianteModel.php
+++ app/Models/EstudianteModel.php
@@ -17,13 +17,17 @@ class EstudianteModel extends Model{
     protected $validationRules  = [
         'nombre'            => 'required|alpha_space|min_length[3]|max_length[75]',
         'apellido'          => 'required|alpha_space|min_length[3]|max_length[75]',
-        
-        'genero'            => 'required|alpha_space|min_length[1]|max_length[1]'
+        'genero'            => 'required|alpha_space|min_length[1]|max_length[1]',
+        'carnet'            => 'required|alpha_numeric|min_length[9]|max_length[9]|"/^(u|U)20[1|2]{1}[0-9]{5}+$/"'
     ];
 
     protected $validationMessages = [
         'genero'        =>[
             'required'      => 'Solo se admite un caracter'
+        ],
+
+        'carnet'        =>[
+            'required'      => 'Solo se admite formato correcto de carnet u20######'
         ],
     ];
 
